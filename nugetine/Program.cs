233d2e11a7cdf5ac00f3fe5897c1d257c4265cb2
Cliff Hammerschmidt <z+gh@hammerschmidt.ca>
2012-05-01 10:35:09 -0700

    inline external refs.

--- nugetine/Program.cs
+++ nugetine/Program.cs
@@ -6,13 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using MongoDB.Bson;
-using Move.Ex.Bson;
-using Move.Ex.DotNet;
+using System.Security.Permissions;
 
 namespace nugetine
 {
     public static class Program
     {
+        [EnvironmentPermissionAttribute(SecurityAction.LinkDemand, Unrestricted = true)]
         public static void Main(string[] args)
         {
             var @out = Console.Out;
@@ -36,6 +36,7 @@ namespace nugetine
                 state.ProcessCsProj);
         }
 
+        [EnvironmentPermissionAttribute(SecurityAction.LinkDemand, Unrestricted = true)]
         private static void DoPackageInstallsAndUpdateGlobalPackagesConfig(TextWriter @out, IState state)
         {
             var globalPackageConfigSb = new StringBuilder();
@@ -99,7 +100,7 @@ namespace nugetine
         public void LoadConfig(string path)
         {
             var source = File.ReadAllText(path);
-            var doc = source.ParseBsonDocument();
+            var doc = BsonDocument.Parse(source);
             _config.Overlay(doc);
         }
 
@@ -198,7 +199,9 @@ namespace nugetine
 
         public override string ToString()
         {
+// ReSharper disable SpecifyACultureInStringConversionExplicitly
             return _config.ToString();
+// ReSharper restore SpecifyACultureInStringConversionExplicitly
         }
     }
 
@@ -209,5 +212,45 @@ namespace nugetine
         void Index();
         IEnumerable<BsonDocument> Packages { get; }
         string Source { get; }
+    }
+
+    internal static class Ex
+    {
+        public static IEnumerable<T> RemoveDuplicatesOn<T, TK>(this IEnumerable<T> enumerable, Func<T, TK> selector)
+        {
+            var seenSet = new HashSet<TK>();
+            foreach(var item in enumerable)
+            {
+                var key = selector(item);
+                if (seenSet.Contains(key)) continue;
+                yield return item;
+                seenSet.Add(key);
+            }
+        }
+
+        public static void Overlay(this BsonDocument bson, BsonDocument with)
+        {
+            foreach (var element in with)
+            {
+                if (bson.Contains(element.Name))
+                {
+                    var original = bson[element.Name];
+                    if (original.BsonType == element.Value.BsonType)
+                    {
+                        if (original.BsonType == BsonType.Document)
+                        {
+                            bson[element.Name].AsBsonDocument.Overlay(element.Value.AsBsonDocument);
+                            continue;
+                        }
+                        if (original.BsonType == BsonType.Array)
+                        {
+                            bson[element.Name] = new BsonArray(original.AsBsonArray.Concat(element.Value.AsBsonArray));
+                            continue;
+                        }
+                    }
+                }
+                bson[element.Name] = element.Value;
+            }
+        }
     }
 }
