e4b04a936205aba6bdad77b5bf81e601f1dc2cc7
Yaniv Haramati <Yaniv.Haramati@move.com>
2012-10-31 13:08:21 -0700

    update

--- NugetFix/AssemblyClassifier/Interface/IClassifier.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace NugetFix.AssemblyClassifier.Interface
-{
-    internal interface IClassifier
-    {
-        void Classify(string dllFilePath, IDictionary<string, IDictionary<string, string>> results);
-    }
-}
